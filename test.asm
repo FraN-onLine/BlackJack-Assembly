ac2f554fef53694202675eb9470b2652ee6f4d51
Franaffel <151704994+FraN-onLine@users.noreply.github.com>
2025-04-02 18:04:31 +0800

    rar

--- test.asm
+++ test.asm
@@ -1,15 +1,66 @@
-.globl
+.data
+
+prompt1: .asciiz "Enter Integer: "
+
+prompt2: .asciiz "Enter Bit Position(0-31): "
+
+output: .asciiz "\nOutput: "
+
 .text
 
-li $t0, 2
-li $t1, 3
-add $t2, $t0, $t1
+.globl main
 
-move $a0, $t2
-li $v0, 1
-syscall
+main:
 
-li $v0, 10
-syscall
+	li $v0, 4
 
-//noted na gumagana yung MIPS dito
+	la $a0, prompt1
+
+	syscall
+
+	li $v0, 5
+
+	syscall
+
+	move $s0, $v0 #store integer input
+
+getBitPosition:
+
+	li $v0, 4
+
+	la $a0, prompt2
+
+	syscall
+
+	li $v0, 5
+
+	syscall
+
+	move $s1, $v0 #store bit position
+
+	bgt $s1, 31, getBitPosition
+
+	
+
+	srlv $t0, $s0, $s1  # Shift right by $s1 bits
+
+    	andi $t0, $t0, 1   # Isolate the least significant bit
+
+    	#this is because a bitwise and with ..0000001 will get the lsb
+
+    	li $v0, 4
+
+	la $a0, output
+
+	syscall
+
+	li $v0, 1
+
+	move $a0, $t0
+
+	syscall
+
+  	li $v0, 10 # Exit program 
+
+	syscall
+
