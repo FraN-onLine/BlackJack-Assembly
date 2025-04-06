d7cebeac8a614df87f5e2e1ca57a395271bc3b23
Franaffel <151704994+FraN-onLine@users.noreply.github.com>
2025-04-06 20:16:35 +0800

    Jack Black "I AM STEVE

--- blackjack.asm
+++ blackjack.asm
@@ -3,40 +3,68 @@
 #github test
 
 .data
-prompt:      .asciiz "\nChoose an option:\n1. Hit\n2. Stand\n3. Run Again\n> "
-winMsg:      .asciiz "\nYou win!\n"
-loseMsg:     .asciiz "\nYou lose.\n"
-drawMsg:     .asciiz "\nDraw.\n"
-hintMsg:     .asciiz "\nCalculating Hint...\n"
-scoreMsg:    .asciiz "\nYour total: "
-cpuScoreMsg: .asciiz "\nDealer total: "
-newline:     .asciiz "\n"
-cardLabel:   .asciiz "Card: "
+prompt:         .asciiz "\nChoose an option:\n1. Hit\n2. Stand\n3. Run Again\n> "
+winMsg:         .asciiz "\nYou win!\n"
+loseMsg:        .asciiz "\nYou lose.\n"
+cardLabel:      .asciiz "Card: "
+suits:          .asciiz "SHCD"         # Spade, Heart, Club, Diamond
+deck:           .asciiz "A23456789TJQK" #T is 10
+newline:        .asciiz "\n"
 
-deck: .asciiz "A23456789TJQK" #T is 10
-deckSize:    .word 52
-playerHand:  .space 52
-cpuHand:     .space 52
-playerCount: .word 0
-cpuCount:    .word 0
+deckSize:       .word 52
+playerHand:     .space 52
+dealerHand:        .space 52
+playerCount:    .word 0
+dealerCount:       .word 0
 
 .text
 .globl main
 
 main:
-    	li   $v0, 42 #randomint
-    	li   $a1, 52  #Draws Between 1-52
-    	syscall            
-	
-	move $v0, $t1
-        rem $t1, 12 #52 % 12 = to loop between Ace to King
-        divu $t2, $v0, 12 #result would be 0-3 mapped with Spade-Heart-Club-Diamonds
-	la $t0, deck #loads the adress of the string
-        add  $t0, $t0, $v0
-	lbu $a0, 0($t0)
-	li $v0, 11
-	syscall
 
-	#a $a0,  prompt
-	#i $v0, 4
-	#yscall
+    li   $v0, 42  #randomint
+    li   $a1, 52  #Draws Between 1-52
+    syscall
+    move $t0, $a0  #move number to $t0
+    
+    #rank determiner
+    li   $t1, 13            #there are 13 ranks
+    divu $t0, $t1
+    mfhi $t2  #HI is the location where the REMAINDER is stored after divu/ div
+
+    #suit determiner
+    mflo $t3   #LO is the location where the QUOTIENT is stored after divu/ div
+
+    la   $t4, deck  #load deck
+    add  $t4, $t4, $t2 #add rank# to deck to determine card
+    lbu  $t5, 0($t4) #loads it to $t5
+
+    #load suits
+    la   $t6, suits 
+    add  $t6, $t6, $t3
+    lbu  $t7, 0($t6) #loads it to $t7
+
+    #label print
+    li   $v0, 4
+    la   $a0, cardLabel
+    syscall
+
+    #print rank
+    li   $v0, 11
+    move $a0, $t5
+    syscall
+
+    #print suit
+    li   $v0, 11
+    move $a0, $t7
+    syscall
+
+    #newline
+    li   $v0, 4
+    la   $a0, newline
+    syscall
+
+    #exit
+    li   $v0, 10
+    syscall
+
