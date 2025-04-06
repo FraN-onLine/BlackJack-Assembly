ecf8959849369b323f7d72c22546a99762fc40c6
Franaffel <151704994+FraN-onLine@users.noreply.github.com>
2025-04-06 09:08:55 +0800

    Skibidi Toilet Rizz

--- blackjack.asm
+++ blackjack.asm
@@ -12,7 +12,7 @@ cpuScoreMsg: .asciiz "\nDealer total: "
 newline:     .asciiz "\n"
 cardLabel:   .asciiz "Card: "
 
-deck: .asciiz "A23"
+deck: .asciiz "A23456789IJQK" #I is 10
 deckSize:    .word 52
 playerHand:  .space 52
 cpuHand:     .space 52
@@ -24,12 +24,14 @@ cpuCount:    .word 0
 
 main:
     	li   $v0, 42 #randomint
-    	li   $a1, 3  #Draws Between 1-52
+    	li   $a1, 52  #Draws Between 1-52
     	syscall            
 	
-	move $v0, $a0
-	 la $t0, deck #loads the adress of the string
-       add  $t0, $t0, $v0
+	move $v0, $t1
+        rem $t1, 12 #52 % 12 = to loop between Ace to King
+        divu $t2, $v0, 12 #result would be 0-3 mapped with Spade-Heart-Club-Diamonds
+	la $t0, deck #loads the adress of the string
+        add  $t0, $t0, $v0
 	lbu $a0, 0($t0)
 	li $v0, 11
 	syscall
